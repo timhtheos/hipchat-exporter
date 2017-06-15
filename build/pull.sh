326e9b523664d17f090fdfabb85e306557564365
timhtheos <me@timothyae.com>
2017-06-15 13:44:05 +0800

    Better logic for pull.sh and build/pull.sh.

--- build/pull.sh
+++ build/pull.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-source config.sh
-source methods.sh
+path=$(pwd)
+
+if [[ $path == *build ]]; then
+  echo "This file - build/pull.sh - should not be executed directly."
+  echo "Please execute pull.sh, and not build/pull.sh."
+  exit 1
+fi
+
+path_build=$path/build
+
+source $path_build/config.sh
+source $path_build/methods.sh
--- pull.sh
+++ pull.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-path=$(pwd)
-path_build=$path/build
-
-# Source config.
-source $path_build/config
+source $(pwd)/build/pull.sh
